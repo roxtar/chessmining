e49d3998d2e8ca6ecda608732c2f92c1a8515bfe
Rohit Kumar <roxtar@gmail.com>
2011-03-01 19:15:17 -0800

    Added TeX files

---
+++ playerelo.R
@@ -0,0 +1,6 @@
+games <- read.csv("playerelo_mar_01_2011_blitz.csv", header=T)
+summary(games$Elo)
+players <- table(games$Player)
+hist(players, breaks=3)
+readline("Hit <Enter> to see the next Plot")
+hist(games$Elo, breaks=20, col="green")
